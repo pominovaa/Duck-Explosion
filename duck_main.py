dd4d5c16333ad77dad021d06e946560ebba776eb
pominovaa <alexandra.pominova.2010@gmail.com>
2021-01-05 18:42:02 -0500

    animation for water and land

--- duck_main.py
+++ duck_main.py
@@ -11,6 +11,10 @@ water_bg = pygame.image.load('Water_BG.png')
 cloud1 = pygame.image.load('Cloud1.png')
 cloud2 = pygame.image.load('Cloud2.png')
 
+land_position_y = 510
+land_speed = 1
+water_position_y = 650
+water_speed = 2
 
 while True:
     for event in pygame.event.get():
@@ -18,10 +22,24 @@ while True:
             pygame.quit()
             sys.exit()
 
-
+    #Background
     screen.blit(wood_bg, (0, 0))
-    screen.blit(land_bg, (0, 560))
-    screen.blit(water_bg, (0, 640))
+    land_position_y += land_speed
+    water_position_y += water_speed
+
+    if land_position_y <= 500 or land_position_y >= 520:
+        land_speed *= -1
+
+    if water_position_y <= 600 or water_position_y >= 660:
+        water_speed *= -1
+
+    screen.blit(land_bg, (0, land_position_y))
+    screen.blit(water_bg, (0, water_position_y))
+
+
+
+
+    #Clouds
     screen.blit(cloud1, (900, 123))
     screen.blit(cloud2, (878, 46))
     screen.blit(cloud1, (342, 146))
